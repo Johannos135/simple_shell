c85f01817b4f13b99fff445334cf1ac5ba253003
Johanne ESSIERE <essierejohanne1@gmail.com>
2023-08-17 01:44:54 -0700

    reverse string function

--- custom_string2.c
+++ custom_string2.c
@@ -1,3 +1,28 @@
 #include "string_fun.h"
 
+/**
+ * reverse_str - this function aims to reverse a string.
+ * @str: this parameter represents the string to reverse.
+ */
+void reverse_str(char *str)
+{
+	int cpt, x, y;
+	char *chaine, temp;
 
+	for (cpt = 0; cpt >= 0; cpt++)
+	{
+		if (str[cpt] == '\0')
+			break;
+	}
+	chaine = str;
+
+	for (x = 0; x < (cpt - 1); x++)
+	{
+		for (y = x + 1; y > 0; y--)
+		{
+			temp = *(chaine + y);
+			*(chaine + y) = *(chaine + (y - 1));
+			*(chaine + (y - 1)) = temp;
+		}
+	}
+}
